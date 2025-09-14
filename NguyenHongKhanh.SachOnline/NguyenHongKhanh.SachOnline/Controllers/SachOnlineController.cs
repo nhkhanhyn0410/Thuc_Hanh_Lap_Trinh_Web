11f88e579abad4dfa3f6d36fa25bfc21be0cd622
Nguyen_Hong_Khanh <nhkyn.0410@gmail.com>
2025-09-14 13:17:29 +0700

    Lap01_Done

---
+++ NguyenHongKhanh.SachOnline/NguyenHongKhanh.SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenHongKhanh.SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NavbarHeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AdsPartial()
+        {
+            return PartialView();
+        }
+    }
+}
